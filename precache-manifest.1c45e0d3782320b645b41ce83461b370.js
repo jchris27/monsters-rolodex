49aa6c588cff8369f67f740e8079d82cf715bee4
Jin <jchris1227@icloud.com>
2020-09-08 12:23:43 +0900

    Updates

---
+++ precache-manifest.1c45e0d3782320b645b41ce83461b370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd53842ab41b186829db0d6011ccbb6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "59681ff48dc475383ff9",
+    "url": "/monsters-rolodex/static/css/main.489ff0a1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59681ff48dc475383ff9",
+    "url": "/monsters-rolodex/static/js/main.e66033aa.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
